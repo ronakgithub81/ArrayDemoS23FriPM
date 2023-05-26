45eb108ff5833c9293598f1cc86615897c771753
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-05-26 08:57:26 -0400

    added comment in student.java

--- src/arraysdemos23fripm/Student.java
+++ src/arraysdemos23fripm/Student.java
@@ -4,7 +4,7 @@ package arraysdemos23fripm;
 public class Student {
     //comment added at 1:21 PM 
     //friday
-    
+    // comment added for modification
     private String sId;
     private String sName;
     private String address;
